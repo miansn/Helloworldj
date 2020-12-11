ef6c0d64ef1768a9d27fa01bdf63123c35cbc1b6
miansn <engr.naeem21@gmail.com>
2020-12-11 22:27:10 +0500

    hello

---
+++ Hello.java
@@ -0,0 +1,20 @@
+public class Hello{
+    
+    
+    public static void main(String[] args){
+        
+        for(int i=1;i<=10;i++){
+            
+            System.out.println("Pakistan..."+i);
+        }
+        
+        
+    }
+    
+    
+
+    
+    
+    
+}
+
